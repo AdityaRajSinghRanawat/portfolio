7348d26cc8f2f24a772de99e08a79e2c60fe8989
Aditya Raj Singh Ranawat <adityarajsinghranawatjobs@gmail.com>
2025-08-28 23:41:44 +0530

    solved the bug caused by the pin animation

--- src/sections/ContactSummary.jsx
+++ src/sections/ContactSummary.jsx
@@ -3,9 +3,11 @@ import Marquee from "../components/Marquee";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 gsap.registerPlugin(ScrollTrigger);
 
 const ContactSummary = () => {
+  const isTablet = useMediaQuery({ maxWidth: "1024px" });
   const containerRef = useRef(null);
 
   const upperMarqueeItems = [
@@ -46,50 +48,86 @@ const ContactSummary = () => {
   }
   useGSAP(() => {
     gsap.to(containerRef.current, {
-      scrollTrigger: {
-        trigger: containerRef.current,
-        start: "center center",
-        end: "+=800 center",
-        scrub: 0.5,
-        pin: true,
-      },
+      scrollTrigger: isTablet
+        ? undefined
+        : {
+            trigger: containerRef.current,
+            start: "center center",
+            end: "+=800 center",
+            scrub: 0.5,
+            pin: true,
+          },
     });
+    isTablet &&
+      gsap.from(containerRef.current, {
+        scrollTrigger: {
+          trigger: ".main-text-section",
+        },
+        delay: 0.5,
+        autoAlpha: 0,
+        y: 200,
+        duration: 1,
+        ease: "circ.out",
+      });
   });
 
   return (
     <section
       ref={containerRef}
-      className="flex flex-col items-center justify-between min-h-[100vh] gap-12 mt-16"
+      className={`flex flex-col items-center h-screen gap-12 ${isTablet ? "mt-0 justify-center" : "mt-16 justify-between"}`}
     >
-      {/* Marquee */}
-      <Marquee
-        icon={upperMarqueeIcon}
-        className={upperMarqueeClass}
-        items={upperMarqueeItems}
-        iconClassName={upperMarqueeIconClass}
-      />
+      {isTablet ? (
+        <>
+          <div></div>
+          {/* Main Text */}
+          <div className="main-text-section text-center font-light overflow-hidden contact-text-responsive py-3 px-5 sm:px-10">
+            <p className="leading-snug">
+              "Transforming <span className="text-gold">ideas</span>
+              <br />
+              into{" "}
+              <span className="text-gold font-kaushan-script">
+                powerful
+              </span>, <span className="italic font-thin">memorable</span>
+              <br />
+              digital <span className="font-bold">identities</span>"
+            </p>
+          </div>
+          <div></div>
+        </>
+      ) : (
+        <>
+          {/* Marquee */}
+          <Marquee
+            icon={upperMarqueeIcon}
+            className={upperMarqueeClass}
+            items={upperMarqueeItems}
+            iconClassName={upperMarqueeIconClass}
+          />
 
-      {/* Main Text */}
-      <div className="text-center font-light overflow-hidden contact-text-responsive py-3 px-5 sm:px-10">
-        <p className="leading-snug">
-          "Transforming <span className="text-gold">ideas</span>
-          <br />
-          into <span className="text-gold font-kaushan-script">
-            powerful
-          </span>, <span className="italic font-thin">memorable</span>
-          <br />
-          digital <span className="font-bold">identities</span>"
-        </p>
-      </div>
+          {/* Main Text */}
+          <div className="text-center font-light overflow-hidden contact-text-responsive py-3 px-5 sm:px-10">
+            <p className="leading-snug">
+              "Transforming <span className="text-gold">ideas</span>
+              <br />
+              into{" "}
+              <span className="text-gold font-kaushan-script">
+                powerful
+              </span>, <span className="italic font-thin">memorable</span>
+              <br />
+              digital <span className="font-bold">identities</span>"
+            </p>
+          </div>
 
-      {/* Marquee */}
-      <Marquee
-        icon={lowerMarqueeIcon}
-        className={lowerMarqueeClass}
-        items={lowerMarqueeItems}
-        iconClassName={lowerMarqueeIconClass}
-        reverse={true}
-      />
+          {/* Marquee */}
+          <Marquee
+            icon={lowerMarqueeIcon}
+            className={lowerMarqueeClass}
+            items={lowerMarqueeItems}
+            iconClassName={lowerMarqueeIconClass}
+            reverse={true}
+          />
+        </>
+      )}
     </section>
   );
 };
